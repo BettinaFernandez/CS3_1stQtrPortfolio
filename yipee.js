ad58d280c45a25fbfc742d8da01c20012a828d25
BettinaFernandez <b2029bfbfernandez@pshs.edu.ph>
2025-09-15 15:02:15 +0800

    commit

---
+++ yipee.js
@@ -0,0 +1,41 @@
+for(let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+fav_fruits = ["mango", "banana", "apple"];
+fav_fruits.push("strawberry");
+var popped = fav_fruits.pop()
+
+var shifted = fav_fruits.shift()
+fav_fruits.unishift("kiwi")
+
+for(let i=0; i < fav_fruits.length; i++){
+    console.log(fav_fruits[i])
+}
+for(fruit of fav_fruits){
+    console.log(fruit)
+}
+fav_fruits.forEach(print_fruit)
+
+i = 1 
+while(i <= 3){
+    console.log(i);
+    i++;
+}
+
+i = 1
+do{
+    console.log(i);
+    i++;
+} while (i <= 3);
+
+
+grades = [90, 80, 99, 98, 97]
+
+sum = 0;
+for(let i = 0; i < grades.length; i++){
+    sum += grades[i]
+}
+console.log(sum)
+
+
