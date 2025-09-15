812fc0b69cba8dbebccb1901c6aa813bec90d19d
BettinaFernandez <b2029bfbfernandez@pshs.edu.ph>
2025-09-15 15:19:08 +0800

    Add files via upload

---
+++ public/arrayfrnds.js
@@ -0,0 +1,6 @@
+let friends = ["Kuch", "Megan", "Raya"];
+console.log(friends);
+friends.push("Meekah");
+console.log("After adding: ", friends);
+friends.pop();
+console.log("After removing: ", friends);
