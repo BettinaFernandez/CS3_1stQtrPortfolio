46eb8d035fbe2528298a70db754458c736de3552
BettinaFernandez <b2029bfbfernandez@pshs.edu.ph>
2025-09-15 15:12:23 +0800

    commit

--- arrayfrnds.js
+++ arrayfrnds.js
@@ -2,3 +2,5 @@ let friends = ["Kuch", "Megan", "Raya"];
 console.log(friends);
 friends.push("Meekah");
 console.log("After adding: ", friends);
+friends.pop();
+console.log("After removing: ", friends);
